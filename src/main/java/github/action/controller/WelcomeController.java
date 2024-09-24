95cedb2f02bf1612c571b27c1267d87a053a568c
ViAarisKea <viaa0001@stud.kea.dk>
2024-09-24 16:27:46 +0200

    change endpoint

--- src/main/java/github/action/controller/WelcomeController.java
+++ src/main/java/github/action/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WelcomeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/")
     public String getWelcome(){
         return "welcome";
     }
