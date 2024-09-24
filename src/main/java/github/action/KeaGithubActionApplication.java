69fd15024bfcb5e142c147a695830099003062e6
ViAarisKea <viaa0001@stud.kea.dk>
2024-09-24 16:15:18 +0200

    test commit

--- src/main/java/github/action/KeaGithubActionApplication.java
+++ src/main/java/github/action/KeaGithubActionApplication.java
@@ -2,6 +2,7 @@ package github.action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KeaGithubActionApplication {
@@ -9,5 +10,7 @@ public class KeaGithubActionApplication {
     public static void main(String[] args) {
         SpringApplication.run(KeaGithubActionApplication.class, args);
     }
+
+
 
 }
--- src/main/java/github/action/controller/WelcomeController.java
+++ src/main/java/github/action/controller/WelcomeController.java
@@ -1,7 +1,10 @@
 package github.action.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class WelcomeController {
 
     @GetMapping("/welcome")
