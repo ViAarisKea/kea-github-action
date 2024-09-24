aff5cded9eda9f6a9adfd3b11fd541d11fdc247f
ViAarisKea <viaa0001@stud.kea.dk>
2024-09-24 14:26:36 +0200

    commit

---
+++ src/main/java/github/action/KeaGithubActionApplication.java
@@ -0,0 +1,13 @@
+package github.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeaGithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeaGithubActionApplication.class, args);
+    }
+
+}
